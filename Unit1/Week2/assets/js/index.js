36d1602169a7a13c0774f0f8d7938d111bc98da0
rositab91 <rositab91@live.it>
2023-04-03 17:07:20 +0200

    Day1

---
+++ Unit1/Week2/assets/js/index.js
@@ -0,0 +1,6 @@
+/*alert('attenzione');
+alert('primo esercizio JavaScript'); */
+
+document.getElementById('title').innerHTML = 'Iniziamo la nostra pratica';
+document.getElementById('output').innerHTML = ('output scritto da');
+
---
+++ Unit1/Week2/assets/js/naming.js
@@ -0,0 +1,13 @@
+let favPet = 'pantera';
+document.write(favPet + ' scritto da Javascript');
+
+let pet1 = 'criceto';
+console.log(pet1);
+
+let pet2 = 'giraffa';
+console.log(pet2);
+
+let pet3 = 'leone';
+console.log(pet3);
+
+
