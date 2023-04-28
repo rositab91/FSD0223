8b02097107d12431b4429ab51975b6d96e5fbf70
rositab91 <rositab91@live.it>
2023-04-28 19:15:57 +0200

    ProgettoSettimana01

---
+++ Unit2/Week1/ProgettoSettimana01/assets/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener('DOMContentLoaded', () => {
+    let scroll = window.scrollY
+
+    const nav = document.querySelector("nav")
+    const scrollChange = 350
+
+    window.addEventListener('scroll', function() { 
+        scroll = window.scrollY;
+        scroll >= scrollChange ? nav.classList.add("white") : nav.classList.remove("white")
+    })
+    })
+document.addEventListener('DOMContentLoaded', () => {
+    let scroll = window.scrollY
+
+    const nav = document.querySelector("button")
+    const scrollChange = 350
+
+    window.addEventListener('scroll', function() { 
+        scroll = window.scrollY;
+        scroll >= scrollChange ? nav.classList.add("green") : nav.classList.remove("green")
+    })
+    })
+
+
+// ANIMAZIONE
+
+let svg = document.querySelectorAll('svg path')
+let animation = setInterval(() => {
+    let random = Math.floor(Math.random() * svg.length)
+    let mAnimation = svg[random]
+    console.log(mAnimation.style.opacity);
+    if(mAnimation.style.opacity && mAnimation.style.opacity === "0"){
+        mAnimation.style.opacity = 1
+    }
+    else{
+        mAnimation.style.opacity = 0
+    }
+}, 100)
+
